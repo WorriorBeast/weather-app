636b24f325fcb04f99c62c862487f84b856b4079
Emmanuel Sanchez <128549074+WorriorBeast@users.noreply.github.com>
2024-10-21 16:58:45 -0700

    Return data object with correct number of days

--- src/query-weather-data.js
+++ src/query-weather-data.js
@@ -3,11 +3,13 @@ async function getWeatherInfo(
 	dynamicPeriod = 7,
 ) {
 	const response = await fetch(
-		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next${dynamicPeriod}days?key=7F8THCGZG9XFKDJDYCETYAEUG`,
+		`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next${dynamicPeriod - 1}days?key=7F8THCGZG9XFKDJDYCETYAEUG`,
 		{ mode: 'cors' },
 	);
 
 	const forecast = await response.json();
+
+	return forecast;
 }
 
 export { getWeatherInfo };

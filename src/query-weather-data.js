023591b240b934f544635b563d5e77f9003ce2de
Emmanuel Sanchez <128549074+WorriorBeast@users.noreply.github.com>
2024-10-23 22:15:15 -0700

    Display error message when api returns error

--- src/query-weather-data.js
+++ src/query-weather-data.js
@@ -1,3 +1,6 @@
+const error = document.getElementById('error-msg');
+const locationInput = document.getElementById('search-bar');
+
 async function getWeatherInfo(
 	location = 'San Francisco,CA',
 	dynamicPeriod = 7,
@@ -7,9 +10,33 @@ async function getWeatherInfo(
 		{ mode: 'cors' },
 	);
 
+	if (response.status == 200) {
+		errorMsg('');
+
+		locationInput.classList.remove('error');
+	} else if (response.status == 400) {
+		errorMsg('Invalid location');
+
+		locationInput.classList.add('error');
+	} else if (response.status == 500) {
+		errorMsg(
+			'Unfortunately an internal server error has occurred. Try again later',
+		);
+
+		locationInput.classList.add('error');
+	} else if (response.status !== 200) {
+		errorMsg('An unexpected error occurred. Please try again');
+
+		locationInput.classList.add('error');
+	}
+
 	const forecast = await response.json();
 
 	return forecast;
 }
+
+const errorMsg = (msg) => {
+	error.textContent = msg;
+};
 
 export { getWeatherInfo };

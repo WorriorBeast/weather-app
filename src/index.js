b53000151cc404184fe4f669a0c2c113969aa08c
Emmanuel Sanchez <128549074+WorriorBeast@users.noreply.github.com>
2024-10-23 18:26:55 -0700

    Enable searchbar for inquiring weather of location

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './style.css';
 import { getClientLocation } from './get-user-location';
 import { toggleSlideIn } from './toggle-slide-effect';
+import { updateWeatherUi } from './update-weather-ui';
 
 const locationInput = document.getElementById('location');
 const periodInput = document.getElementById('period');
 const toggleWeatherReport = document.querySelector('.toggle-weather-report');
+const searchBar = document.getElementById('search-bar');
 
 document.addEventListener('DOMContentLoaded', getClientLocation);
 
@@ -15,3 +17,12 @@ periodInput.addEventListener('focus', toggleSlideIn);
 periodInput.addEventListener('blur', toggleSlideIn);
 
 toggleWeatherReport.addEventListener('click', toggleSlideIn);
+
+searchBar.addEventListener('submit', (e) => {
+	let location = locationInput.value;
+	let period = periodInput.value;
+
+	if (period.trim() == '') period = undefined;
+
+	updateWeatherUi(e, location, period);
+});

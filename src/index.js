80e06954848cbc8b4cd36c8fc33f15e6dedec328
Emmanuel Sanchez <128549074+WorriorBeast@users.noreply.github.com>
2024-10-23 18:10:54 -0700

    Toggle weather report into view

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { toggleSlideIn } from './toggle-slide-effect';
 
 const locationInput = document.getElementById('location');
 const periodInput = document.getElementById('period');
+const toggleWeatherReport = document.querySelector('.toggle-weather-report');
 
 document.addEventListener('DOMContentLoaded', getClientLocation);
 
@@ -12,3 +13,5 @@ locationInput.addEventListener('blur', toggleSlideIn);
 
 periodInput.addEventListener('focus', toggleSlideIn);
 periodInput.addEventListener('blur', toggleSlideIn);
+
+toggleWeatherReport.addEventListener('click', toggleSlideIn);
